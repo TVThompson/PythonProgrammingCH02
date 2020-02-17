09578bbb092dae6171207b06cafc5c082889a716
TVThompson <timthompson@timthompsonswebsite.com>
2020-02-17 09:39:20 -0800

    Initial commit: Tested.

---
+++ convert.py
@@ -0,0 +1,16 @@
+# convert.py
+# A program to convert temperature in degrees Celsius to temperature in degrees
+# Fahrnheit
+# From Python Programming Chapter 2
+
+
+def main():
+    # Input temperature in degrees C
+    celsius = float(input('Enter the temperature in degrees C: '))
+    # Convert using F = 9/5*C+32
+    fahrenheit = 9 / 5 * celsius + 32
+    # Output temperature in degrees F
+    print(fahrenheit)
+
+
+main()
